5f3f8553eb0c871b8565902c99f3e10f9aea71bc
Marius Talpos <andreitalpos@yahoo.com>
2016-01-23 17:20:44 -0500

    made contact form with ajax styled contact form (not done) fixed contactForm.php path set body background and fixed wrapper div (98%)

--- contactForm.php
+++ contactForm.php
@@ -1,14 +1,25 @@
-<form>
-	Name:<br>
-	<input type='text' Name='name'><br>
-	<input type='text' Name='name'><br>
-	<input type='checkbox'>
-	<input type='checkbox'>
-	<input type='checkbox'>
-	<select>
-		<option value="value 1" selected>Value 1</option>
-		<option value="value 2">Value 2</option>
-		<option value="value 3">Value 3</option>
-	</select>
-	<input type='submit' value='Submit'>
-</form>
+<?php
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+
+
+$email_from = 'andreitalpos@yahoo.com';
+$email_subject = 'New Form Submission';
+$email_body = '$name. has sent youa email from your webiste \n\r $message' ;
+
+
+
+$to = 'andreitalpos@yahoo.com';
+$headers = 'From: $email_from \n';
+$headers = 'Reply-To: $visitor_email \n';
+
+if(mail($to, $email_subject, $email_body, $headers)){
+	$response = 'Your mail has been successfully sent.';
+} else {
+	$response = 'Opps there was a problem submitting your mail. Please try again later.';
+}
+
+echo $response;
--- js/myScripts/myScript.js
+++ js/myScripts/myScript.js
@@ -2,4 +2,31 @@ jQuery( document ).ready(function( $ ) {
     $(window).load(function() {
         $('#slider').nivoSlider();
     });
+
+	var form = $('#email-form');
+	var formMessages = $('#form-messages');
+
+	$(form).submit(function(evt){
+		evt.preventDefault();
+	});
+
+	var formData = $(form).serialize();
+
+	var contactPromise = $.ajax({
+		type: 'POST',
+		url: $(form).attr('action'),
+		data: formData
+	});
+
+	contactPromise.done(function(response){
+		$(formMessages).removeClass('error');
+	    $(formMessages).addClass('success');
+	    
+	    $(formMessages).text(response);
+	    
+	    $('#name').val('');
+	    $('#email').val('');
+	    $('#message').val('');
+	});
 });
+
